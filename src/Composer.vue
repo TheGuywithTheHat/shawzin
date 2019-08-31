d591200741b8505d55f89582b7cff1ca7f1d533d
randy <randycarlson486@gmail.com>
2019-08-31 18:02:31 -0400

    made notes togglable

--- src/Composer.vue
+++ src/Composer.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="content">
         <table id="header-table">
-            <tr class="row note-row" v-for="(row, y) in cells" :key="row[0].y + 'row'">
+            <tr class="row" v-for="(row, y) in cells" :key="y + 'row'">
                 <td class="cell header-cell">
                     {{notes[y]}}
                 </td>
@@ -9,11 +9,16 @@
         </table>
         <div id="music-container">
             <table id="music-table">
-                <tr class="row note-row" v-for="(row, y) in cells" :key="row[0].y + 'row'">
-                    <td class="cell note-cell" v-for="(cell, x) in row" :key="cell.y + cell.x * 100 + 'cell'"></td>
+                <tr class="row note-row" v-for="(row, y) in cells" :key="y + 'row'">
+                    <td class="cell note-cell"
+                        :class="{ 'note-on': cells[y][x] }"
+                        v-for="(cell, x) in row"
+                        :key="y + x * 100 + 'cell'"
+                        @click="click(x, y)"></td>
                 </tr>
             </table>
         </div>
+        <input class="pure-button pure-button-primary" type="button" value="Copy to clipboard" @click="copy()">
     </div>
 </template>
 
@@ -26,10 +31,23 @@ export default {
         };
     },
     methods: {
+        copy() {
+            navigator.clipboard.writeText(this.getEncoding());
+        },
+        getEncoding() {
+            return 'test successful';
+        },
+        click(x, y) {
+            this.$set(this.cells[y], x, !this.cells[y][x]);
+        },
     },
     created() {
-        for(let i = 0; i < 12; i++) {
-            this.cells.push(new Array(16).fill(false))
+        let initialSize = 32;
+        for(let y = 0; y < 12; y++) {
+            this.$set(this.cells, y, []);
+            for(let x = 0; x < initialSize; x++) {
+                this.$set(this.cells[y], x, false);
+            }
         }
     }
 };
@@ -37,27 +55,38 @@ export default {
 
 <style scoped>
 #header-table {
-    margin: 1em;
+    margin: 1em 0.5em 1em 0;
     float: left;
 }
+
 #music-table {
 }
+
 #music-container {
     margin: 1em;
     overflow-x: auto;
 }
+
 .note-cell {
     min-width: 20px;
-    border: 1px solid rgb(168, 168, 168);
+    border: 0px solid rgb(128, 128, 128);
     vertical-align: top;
     position: relative;
     cursor: pointer;
 }
+
+.content {
+    padding: 1em;
+}
+
 .note-cell:first-child {
-    border-left: 2px solid;
+    border-left: 3px solid rgb(211, 211, 211);
 }
 .note-cell:nth-child(4n) {
-    border-right: 2px solid;
+    border-right-width: 1px;
+}
+.note-cell:nth-child(16n) {
+    border-right: 3px solid rgb(211, 211, 211);
 }
 .note-cell:hover {
     background-color: rgba(255, 255, 255, 0.15);
@@ -65,7 +94,19 @@ export default {
 .row {
     height: 30px;
 }
+.note-row {
+    border: 1px solid rgb(128, 128, 128);
+}
+.note-row:hover {
+    background-color: rgba(255, 255, 255, 0.1);
+}
 .note-row-sharp {
     height: 20px;
+}
+.note-on {
+    background-color: white;
+}
+.note-on:hover {
+    background-color: rgb(184, 184, 184);
 }
 </style>
